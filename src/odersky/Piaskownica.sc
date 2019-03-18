a430112f896cebbbbfb94b761a05df9ae6535097
krzyzakov <krzyzakov@gmail.com>
2019-03-18 20:40:42 +0100

    Przed merge

---
+++ src/odersky/Piaskownica.sc
@@ -0,0 +1,6 @@
+import odersky.Rational
+
+new Rational(1, 2)
+new Rational(1, 3)
+
+val z = new Rational(2, 3)
---
+++ src/odersky/Rational.scala
@@ -0,0 +1,5 @@
+package odersky
+
+class Rational(n: Int, d: Int) {
+    override def toString: String = n + "/" + d
+}
